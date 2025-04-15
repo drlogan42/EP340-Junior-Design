d9058d73639e492004f99183edd939ca85a60e54
Logan Shaffer <drlogan42@gmail.com>
2025-04-15 17:43:34 -0400

    Create InitializationTest.ino
    Screen initialization setup process prior to PID and Inverse Kinematics

---
+++ InitializationTest.ino
@@ -0,0 +1,99 @@
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include <TouchScreen.h>
+
+// --- Motor Pins ---
+#define MOTOR_A_IN1  2
+#define MOTOR_A_IN2  3
+#define MOTOR_A_IN3  4
+#define MOTOR_A_IN4  5
+
+#define MOTOR_B_IN1  6
+#define MOTOR_B_IN2  7
+#define MOTOR_B_IN3  8
+#define MOTOR_B_IN4  9
+
+#define MOTOR_C_IN1 10
+#define MOTOR_C_IN2 11
+#define MOTOR_C_IN3 12
+#define MOTOR_C_IN4 13
+
+// --- Touchscreen Pins ---
+#define XP A1
+#define XM A2
+#define YP A3
+#define YM A0
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 480);
+
+// --- Touchscreen Calibration ---
+#define TS_MINX 70
+#define TS_MAXX 510
+#define TS_MINY 470
+#define TS_MAXY 930
+
+#define SCREEN_WIDTH_MM 130
+#define SCREEN_HEIGHT_MM 172
+
+// --- Stepper Setup ---
+#define STEPS_PER_REV 400
+#define MAX_MOTOR_SPEED 300
+#define MOTOR_ACCELERATION 100
+
+AccelStepper stepperA(AccelStepper::HALF4WIRE, MOTOR_A_IN1, MOTOR_A_IN2, MOTOR_A_IN3, MOTOR_A_IN4);
+AccelStepper stepperB(AccelStepper::HALF4WIRE, MOTOR_B_IN1, MOTOR_B_IN2, MOTOR_B_IN3, MOTOR_B_IN4);
+AccelStepper stepperC(AccelStepper::HALF4WIRE, MOTOR_C_IN1, MOTOR_C_IN2, MOTOR_C_IN3, MOTOR_C_IN4);
+
+MultiStepper steppers;
+
+// --- Initialization State ---
+bool systemReady = false;
+long initialOffset[3] = {0, 0, 0};
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Ball Balancer setup started...");
+
+  stepperA.setMaxSpeed(MAX_MOTOR_SPEED);
+  stepperB.setMaxSpeed(MAX_MOTOR_SPEED);
+  stepperC.setMaxSpeed(MAX_MOTOR_SPEED);
+
+  stepperA.setAcceleration(MOTOR_ACCELERATION);
+  stepperB.setAcceleration(MOTOR_ACCELERATION);
+  stepperC.setAcceleration(MOTOR_ACCELERATION);
+
+  steppers.addStepper(stepperA);
+  steppers.addStepper(stepperB);
+  steppers.addStepper(stepperC);
+
+  // Slight lift to get platform off base
+  long initialLift[3] = {100, 100, 100};
+  steppers.moveTo(initialLift);
+  steppers.runSpeedToPosition();
+
+  Serial.println("Platform lifted. Tap the screen to begin...");
+}
+
+void loop() {
+  if (!systemReady) {
+    TSPoint p = ts.getPoint();
+
+    pinMode(XP, OUTPUT);
+    pinMode(XM, OUTPUT);
+    pinMode(YP, OUTPUT);
+    pinMode(YM, OUTPUT);
+
+    if (p.z > 10) {
+      initialOffset[0] = stepperA.currentPosition();
+      initialOffset[1] = stepperB.currentPosition();
+      initialOffset[2] = stepperC.currentPosition();
+
+      systemReady = true;
+      Serial.println("Touch detected. Ready to begin control...");
+    }
+
+    return;
+  }
+
+  // Control logic not yet implemented here
+}
